b721dcb9b31df3d6b9511fe0aeba9bc74d2c9968
DarraghMcMullen <dmcm9019@uni.sydney.edu.au>
2021-03-09 20:33:22 +1100

    updated main.asm in exercise 3

--- lab_assessment_1/exercise3/Sources/main.asm
+++ lab_assessment_1/exercise3/Sources/main.asm
@@ -21,11 +21,15 @@ ROMStart    EQU  $4000  ; absolute address to place my code/constant data
 
 ; variable/data section
 
+ ifdef _HCS12_SERIALMON
+            ORG $3FFF - (RAMEnd - RAMStart)
+ else
             ORG RAMStart
+ endif
  ; Insert here your data definition.
 Counter     DS.W 1
 FiboRes     DS.W 1
-
+WORD FCB $4D,$65,$6D,$6F,$72,$79,$0D,$00
 
 ; code section
             ORG   ROMStart
@@ -33,31 +37,42 @@ FiboRes     DS.W 1
 
 Entry:
 _Startup:
+            ; remap the RAM &amp; EEPROM here. See EB386.pdf
+ ifdef _HCS12_SERIALMON
+            ; set registers at $0000
+            CLR   $11                  ; INITRG= $0
+            ; set ram to end at $3FFF
+            LDAB  #$39
+            STAB  $10                  ; INITRM= $39
+
+            ; set eeprom to end at $0FFF
+            LDAA  #$9
+            STAA  $12                  ; INITEE= $9
+
+
+            LDS   #$3FFF+1        ; See EB386.pdf, initialize the stack pointer
+ else
             LDS   #RAMEnd+1       ; initialize the stack pointer
+ endif
 
             CLI                     ; enable interrupts
-mainLoop:
-            LDX   #1              ; X contains counter
-couterLoop:
-            STX   Counter         ; update global.
-            BSR   CalcFibo
-            STD   FiboRes         ; store result
-            LDX   Counter
-            INX
-            CPX   #24             ; larger values cause overflow.
-            BNE   couterLoop
-            BRA   mainLoop        ; restart.
 
-CalcFibo:  ; Function to calculate fibonacci numbers. Argument is in X.
-            LDY   #$00            ; second last
-            LDD   #$01            ; last
-            DBEQ  X,FiboDone      ; loop once more (if X was 1, were done already)
-FiboLoop:
-            LEAY  D,Y             ; overwrite second last with new value
-            EXG   D,Y             ; exchange them -> order is correct again
-            DBNE  X,FiboLoop
-FiboDone:
-            RTS                   ; result in D
+  
+mainLoop:
+  LDX #WORD  
+  LOOP:
+    LDAA X
+    INX
+    CMPA $00
+    BEQ test
+    bra LOOP
+    
+test:
+          ldaa #00
+          bra mainLoop
+  
+  
+
 
 ;**************************************************************
 ;*                 Interrupt Vectors                          *
